edc6e61fa2457c60b9115474edf330797a52c697
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-02 22:08:03 -0400

    Set up and configure MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -6,6 +7,16 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI_DEV;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
