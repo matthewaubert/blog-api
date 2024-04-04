6e3660845906663e847308ec32fa7f408c5c7248
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-04 14:35:31 -0400

    Handle errors in 'slugify' func

--- utils/util.js
+++ utils/util.js
@@ -6,25 +6,29 @@ const limax = require('limax'); // https://github.com/lovell/limax
  * and numbers are appended to non-unique titles
  * @param {String} str - string to be transformed to slug
  * @param {String} model - Mongoose Model to check for uniqueness
- * @returns {String}
+ * @returns {Promise<string>}
  * e.g. 'The Quick Brown Fox Jumps Over The Lazy Dog! ' => 'the-quick-brown-fox-jumps-over-the-lazy-dog'
  * e.g. 'söme stüff with áccènts' => 'some-stuff-with-accents'
  * e.g. 'söme stüff with áccènts' => 'some-stuff-with-accents-1'
  */
 exports.slugify = async (str, model) => {
   const slug = limax(str);
-
-  // require correct model
-  const Model = require(`../models/${model}`); // eslint-disable-line
-
-  // check for uniqueness
   let uniqueSlug = slug;
-  let existingPost = await Model.findOne({ slug: uniqueSlug });
-  let count = 1;
-  while (existingPost) {
-    uniqueSlug = `${slug}-${count}`;
-    existingPost = await Model.findOne({ slug: uniqueSlug }); // eslint-disable-line no-await-in-loop
-    count++;
+  
+  try {
+    // require correct model
+    const Model = require(`../models/${model}`); // eslint-disable-line
+    
+    // check for uniqueness
+    let existingPost = await Model.findOne({ slug: uniqueSlug });
+    let count = 1;
+    while (existingPost) {
+      uniqueSlug = `${slug}-${count}`;
+      existingPost = await Model.findOne({ slug: uniqueSlug }); // eslint-disable-line no-await-in-loop
+      count++;
+    }
+  } catch (err) {
+    console.error(err);
   }
 
   // console.log(uniqueSlug);
