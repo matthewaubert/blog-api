f5436866fe687d8fe87254f33f7f24636051b49a
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-04 14:32:17 -0400

    Build error handling middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const createError = require('http-errors'); // https://www.npmjs.com/package/http-errors
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -27,5 +28,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // return error
+  res.status(err.status || 500).json({
+    message: err.status
+      ? `${err.status} ${err.message}`
+      : `${res.status} Internal Server Error`,
+    errors: [err],
+  });
+});
 
 module.exports = app;
