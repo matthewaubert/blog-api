ff3ed3592fd65932b97af040b6d2508a586995be
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-04 14:44:04 -0400

    Write basic controller for User post

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,7 @@
+const User = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const { slugify } = require('../utils/util');
+
 // GET all Users
 exports.getAll = (req, res) => {
   res.json({ message: 'NOT IMPLEMENTED: GET all Users' });
@@ -12,9 +16,32 @@ exports.getOne = (req, res) => {
 };
 
 // POST (create) a new User
-exports.post = (req, res) => {
-  res.json({ message: 'NOT IMPLEMENTED: POST (create) a new User' });
-};
+exports.post = [
+  // validate and sanitize User fields
+
+  asyncHandler(async (req, res) => {
+    console.log('req.body:', req.body);
+    // extract validation errors from request
+
+    // generate encrypted password w/ bcrypt
+
+    // create a User object w/ escaped & trimmed data
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      slug: await slugify(req.body.username, 'user'),
+      email: req.body.email,
+      password: req.body.password, // need to hash this
+    });
+
+    // if errors: send user and errors back as JSON?
+
+    // data from form is valid. Save User and send back as JSON.
+    await user.save();
+    res.json(user);
+  }),
+];
 
 // PUT (update) a User
 exports.put = (req, res) => {
