333aa7781d78a167ae1d5fcad6f576f3aafa2e2a
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-17 15:43:59 -0400

    Refactor 'validateCommentIdParam' func

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 const { isValidObjectId } = require('mongoose');
 const createError = require('http-errors'); // https://www.npmjs.com/package/http-errors
 const asyncHandler = require('express-async-handler'); // https://www.npmjs.com/package/express-async-handler
@@ -20,20 +21,28 @@ exports.validateIdParam = (req, res, next) => {
 
 /**
  * Express middleware: throw error if `req.params.commentId` is an invalid MongoDB object id
+ * or given Comment doesn't belong to given Post
  * @param {object} req - Express `request` object
  * @param {object} res - Express `response` object
  * @param {function} next - Express `next` function
  * @returns {undefined}
  */
-exports.validateCommentIdParam = (req, res, next) => {
-  // if invalid id given: throw error
-  if (!isValidObjectId(req.params.commentId))
-    return next(
-      createError(404, `Invalid comment id: ${req.params.commentId}`),
-    );
+exports.validateCommentIdParam = asyncHandler(async (req, res, next) => {
+  const { postId, commentId } = req.params;
+  let isValid = true;
 
+  if (!isValidObjectId(commentId)) isValid = false;
+  const comment = isValid
+    ? await Comment.findOne({ _id: commentId, post: postId }).exec()
+    : null;
+  if (!comment) isValid = false;
+
+  // if invalid commentId given: throw error
+  if (!isValid) {
+    return next(createError(404, `Invalid comment id: ${commentId}`));
+  }
   return next();
-};
+});
 
 /**
  * Express middleware: throw error if `req.params.postId` is an invalid MongoDB object id
@@ -51,9 +60,9 @@ exports.validatePostIdParam = asyncHandler(async (req, res, next) => {
   const post = isValid ? await Post.findById(postId).exec() : null;
   if (!post) isValid = false;
 
+  // if invalid postId given: throw error
   if (!isValid) {
-    next(createError(404, `Invalid post id: ${postId}`));
-  } else {
-    return next();
+    return next(createError(404, `Invalid post id: ${postId}`));
   }
+  return next();
 });
