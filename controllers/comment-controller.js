7ed037d972363e0f9729abe453aa6e6a30fcbab7
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-04-17 14:02:49 -0400

    Write Comment GET one controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,6 +2,7 @@ const Comment = require('../models/comment');
 const User = require('../models/user');
 const Post = require('../models/post');
 const { isValidObjectId } = require('mongoose');
+const createError = require('http-errors'); // https://www.npmjs.com/package/http-errors
 const asyncHandler = require('express-async-handler'); // https://www.npmjs.com/package/express-async-handler
 const { body, validationResult } = require('express-validator'); // https://express-validator.github.io/docs
 const { encode } = require('he'); // https://www.npmjs.com/package/he
@@ -36,9 +37,25 @@ exports.getAll = asyncHandler(async (req, res) => {
 });
 
 // GET a single Comment
-exports.getOne = (req, res) => {
-  res.json({ message: 'NOT IMPLEMENTED: GET a single Comment' });
-};
+exports.getOne = asyncHandler(async (req, res, next) => {
+  // get Comment w/ `_id` that matches `req.params.commentId`
+  // and `post` that matches `req.params.postId`
+  const comment = await Comment.findOne({
+    _id: req.params.commentId,
+    post: req.params.postId,
+  })
+    .populate('user', 'firstName lastName username slug')
+    .populate('post', 'title slug')
+    .exec();
+
+  // if Comment not found: throw error
+  if (!comment) return next(createError(404, 'Comment not found'));
+
+  res.json({
+    message: `Comment '${comment.id}' fetched from database`,
+    data: comment,
+  });
+});
 
 // validation & sanitization chain for Comment POST & PUT
 const validationChainPostPut = [
@@ -87,7 +104,7 @@ exports.post = [
       // data from form is valid. Save Comment and send back as JSON.
       await comment.save();
       res.json({
-        message: 'Comment saved to database',
+        message: `Comment '${comment.id}' saved to database`,
         data: comment,
       });
     }
