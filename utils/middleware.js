0148008c4d4cb15632ee74b2481b68d74b40b80e
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2024-05-08 19:22:38 -0400

    Refactor ID validation middleware for Comment

--- utils/middleware.js
+++ utils/middleware.js
@@ -24,33 +24,8 @@ exports.validateIdParam = (req, res, next) => {
 };
 
 /**
- * Express middleware: throw error if `req.params.commentId` is an invalid MongoDB object id
- * or given Comment doesn't belong to given Post
- * @param {object} req - Express `request` object
- * @param {object} res - Express `response` object
- * @param {function} next - Express `next` function
- * @returns {undefined}
- */
-exports.validateCommentIdParam = asyncHandler(async (req, res, next) => {
-  const { postId, commentId } = req.params;
-  let isValid = true;
-
-  if (!isValidObjectId(commentId)) isValid = false;
-  const comment = isValid
-    ? await Comment.findOne({ _id: commentId, post: postId }).exec()
-    : null;
-  if (!comment) isValid = false;
-
-  // if invalid commentId given: throw error
-  if (!isValid) {
-    return next(createError(404, `Invalid comment id: ${commentId}`));
-  }
-  return next();
-});
-
-/**
- * Express middleware: throw error if `req.params.postId` is an invalid MongoDB object id
- * or doesn't belong to an existing Post
+ * Express middleware: throw error if `req.params.postId` doesn't belong to an existing Post.
+ * Normalize `req.params.id` to a valid Post id.
  * @param {object} req - Express `request` object
  * @param {object} res - Express `response` object
  * @param {function} next - Express `next` function
@@ -58,15 +33,39 @@ exports.validateCommentIdParam = asyncHandler(async (req, res, next) => {
  */
 exports.validatePostIdParam = asyncHandler(async (req, res, next) => {
   const { postId } = req.params;
-  let isValid = true;
 
-  if (!isValidObjectId(postId)) isValid = false;
-  const post = isValid ? await Post.findById(postId).exec() : null;
-  if (!post) isValid = false;
+  // create MongoDB query obj based on request param
+  const mongoDbPostQuery = isValidObjectId(postId)
+    ? { _id: postId }
+    : { slug: postId };
 
   // if invalid postId given: throw error
-  if (!isValid) {
-    return next(createError(404, `Invalid post id: ${postId}`));
-  }
+  const post = await Post.findOne(mongoDbPostQuery).exec();
+  if (!post) return next(createError(404, `Post ${postId} not found`));
+
+  // normalize postId to an id (not a slug)
+  req.params.postId = post._id;
+  return next();
+});
+
+/**
+ * Express middleware: throw error if `req.params.commentId` doesn't belong to
+ * an existing Comment. Set `req.mongoDbQuery` property, an object used to query
+ * the MongoDB database, based on client-provided `postId` and `commentId`.
+ * @param {object} req - Express `request` object
+ * @param {object} res - Express `response` object
+ * @param {function} next - Express `next` function
+ * @returns {undefined}
+ */
+exports.validateCommentIdParam = asyncHandler(async (req, res, next) => {
+  const { postId, commentId } = req.params;
+
+  // create MongoDB query obj based on request params
+  req.mongoDbQuery = { _id: commentId, post: postId };
+
+  const comment = await Comment.findOne(req.mongoDbQuery).exec();
+  // if invalid commentId given: throw error
+  if (!comment) return next(createError(404, `Comment ${commentId} not found`));
+
   return next();
 });
